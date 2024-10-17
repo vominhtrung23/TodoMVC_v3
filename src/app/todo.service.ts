60d616871a94834756c9331f74551fc0ecd1438f
vominhtrung23 <vominhtrung23@gmail.com>
2024-10-17 10:08:40 +0700

    save

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,79 +1,101 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
-import { BehaviorSubject, combineLatest, map } from 'rxjs';
+import { BehaviorSubject, delay, map } from 'rxjs';
 import { v4 as uuidv4 } from 'uuid';
 
 import { FilterType, Todo } from './models';
+
+// type State = {
+//   items: Todo[];
+//   filter: FilterType;
+// };
+
+// const initialState = { items: [] as Todo[], filter: 'All' as FilterType };
+// type State = typeof initialState;
+
+type State = {
+  items: Todo[];
+  filter: FilterType;
+  isLoading: boolean;
+};
+
+const initialState: State = { items: [], filter: 'All', isLoading: false };
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-  private items$ = new BehaviorSubject<Todo[]>([]);
-  private filterType$ = new BehaviorSubject<FilterType>('All');
+  private state$ = new BehaviorSubject<State>(initialState);
   private http = inject(HttpClient);
 
-  vm$ = combineLatest([this.items$, this.filterType$]).pipe(
-    map(([items, filter]) => {
+  vm$ = this.state$.pipe(
+    map((state) => {
       return {
-        filteredItems: getFilterItems(items, filter),
-        filter,
-        activeCount: items.filter((x) => !x.completed).length,
+        filteredItems: getFilterItems(state),
+        activeCount: state.items.filter((x) => !x.completed).length,
+        filter: state.filter,
+        isLoading: state.isLoading,
       };
     })
   );
 
+  private patchState(newState: Partial<State>) {
+    this.state$.next({ ...this.state$.value, ...newState });
+  }
+
   load() {
+    this.patchState({ isLoading: true });
     this.http
       .get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .pipe(delay(3000))
       .subscribe((res) => {
-        this.items$.next(res);
+        this.patchState({ items: res, isLoading: false });
       });
   }
 
   add(value: string) {
-    const current = this.items$.getValue();
     const todo: Todo = {
       id: uuidv4(),
       title: value,
       completed: false,
     };
-    const nextValue = [...current, todo];
-    this.items$.next(nextValue);
+    const newItems = [...this.state$.value.items, todo];
+    this.patchState({ items: newItems });
   }
 
   remove(todo: Todo) {
-    const nextValue = this.items$.getValue().filter((x) => x.id !== todo.id);
-    this.items$.next(nextValue);
+    const newItems = this.state$.value.items.filter((x) => x.id !== todo.id);
+    this.patchState({ items: newItems });
   }
 
   update(todo: Todo) {
-    const nextValue = this.items$
-      .getValue()
-      .map((x) => (x.id === todo.id ? { ...x, completed: !x.completed } : x));
-    this.items$.next(nextValue);
+    const newItems = this.state$.value.items.map((x) =>
+      x.id === todo.id ? { ...x, completed: !x.completed } : x
+    );
+    this.patchState({ items: newItems });
   }
 
   toggleAll(isActive: boolean) {
-    const nextValue = this.items$
-      .getValue()
-      .map((item: Todo) => ({ ...item, completed: isActive }));
-    this.items$.next(nextValue);
+    const newItems = this.state$.value.items.map((item: Todo) => ({
+      ...item,
+      completed: isActive,
+    }));
+    this.patchState({ items: newItems });
   }
 
   filter(filtering: FilterType) {
-    this.filterType$.next(filtering);
+    this.patchState({ filter: filtering });
   }
 
   clearCompleted() {
-    const nextValue = this.items$.getValue().filter((x) => !x.completed);
-    this.items$.next(nextValue);
+    const newItems = this.state$.value.items.filter((x) => !x.completed);
+    this.patchState({ items: newItems });
   }
 }
 
 // computed
-function getFilterItems(items: Todo[], filter: FilterType) {
+function getFilterItems({ items, filter }: State) {
   switch (filter) {
     case 'Active':
       return items.filter((item) => !item.completed);
